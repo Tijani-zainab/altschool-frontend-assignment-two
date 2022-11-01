f224dfa6bb54723a79c9135f9c5f20fb2f40b0ee
Zainab Tijani <jani.tijj@gmail.com>
2022-11-02 00:36:35 +0100

    fix nested route secret page, still shows on about page. fix later

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,10 +1,23 @@
+// import { lazy } from 'react';
 import './About.scss';
 import { HashLink } from 'react-router-hash-link';
 import { Outlet } from 'react-router-dom';
 import AboutArrowCard from '../../assets/images/about-arrow-card.jpg';
+import { SecretAbout } from '../../Pages';
+// let SecretAbout = lazy(() => import('./SecretAbout'));
 
 
-const About = () => {
+const About = ({isSecret}) => {  
+    
+    if (isSecret) {
+        return (
+            <div className='about' id='about'>
+                { SecretAbout }
+            </div>
+        );
+    }
+    
+
     return (
         <div className='about-page' id='about'>
 
@@ -16,13 +29,17 @@ const About = () => {
 
                 <section className='about-contact__desc'>
                     <p>This is a simple page with a few pages and a few components.</p>
-                    <HashLink className='hashlink-link secret-link' smooth to="secret-about/#secret" scroll={(el) => el.scrollIntoView({ behavior: 'auto', block: 'end' })}> Secret page </HashLink>
+                    <HashLink className='secret-link' smooth to="#secret" scroll={(el) => el.scrollIntoView({ behavior: 'auto', block: 'end' })}> Secret page </HashLink>
                 </section>
+                
+
+                <SecretAbout isSecret />
             </div>
             
             <Outlet />
         </div>
     );
+    
 }
 
 export default About;
--- src/Pages/About/SecretAbout.js
+++ src/Pages/About/SecretAbout.js
@@ -1,13 +1,15 @@
 import './SecretAbout.scss';
 import { useParams } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
 
 const SecretAbout = () => {
 
     let { secretId } = useParams();
 
     return (
-        <div className='secret-about-page'>
+        <div className='secret-about-page' id='secret'>
             <h1>Secret nested About information in the about page ! {secretId}</h1>
+            <HashLink className='secret-link' smooth to="#about" scroll={(el) => el.scrollIntoView({ behavior: 'auto', block: 'end' })}> Back to About </HashLink>
         </div>
     );
 }
